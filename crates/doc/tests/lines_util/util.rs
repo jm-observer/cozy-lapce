5040083ca6af4f8087437653adbc218ef21a1dee
fengqi <362254883@qq.com>
2025-02-27 08:31:51 +0800

    1. stash test test_phantom

--- crates/doc/tests/lines_util/util.rs
+++ crates/doc/tests/lines_util/util.rs
@@ -1,28 +1,27 @@
 use doc::lines::phantom_text::{PhantomTextMultiLine, Text, combine_with_text};
 use smallvec::SmallVec;
 
-pub fn check_lines_col(
-    lines: &SmallVec<[Text; 6]>,
-    final_text_len: usize,
-    origin: &str,
-    expect: &str
-) {
-    let rs = combine_with_text(lines, origin);
-    assert_eq!(expect, rs.as_str());
-    assert_eq!(final_text_len, expect.len());
+#[macro_export] macro_rules! check_lines_col {
+    ($lines:expr, $final_text_len:expr, $origin:expr, $expect:expr) => {
+        let rs = combine_with_text($lines, $origin);
+        assert_eq!(rs.as_str(), $expect);
+        assert_eq!($final_text_len, $expect.len());
+    };
 }
 
-pub fn check_line_final_col(lines: &PhantomTextMultiLine, rs: &str) {
-    for text in &lines.text {
+#[macro_export] macro_rules! check_line_final_col {
+    ($lines:expr, $rs:expr) => {
+        for text in &$lines.text {
         if let Text::Phantom { text } = text {
             assert_eq!(
                 text.text.as_str(),
-                sub_str(rs, text.final_col, text.final_col + text.text.len())
+                sub_str($rs, text.final_col, text.final_col + text.text.len())
             );
         }
     }
+    };
 }
 
-fn sub_str(text: &str, begin: usize, end: usize) -> &str {
+pub fn sub_str(text: &str, begin: usize, end: usize) -> &str {
     unsafe { text.get_unchecked(begin..end) }
 }
